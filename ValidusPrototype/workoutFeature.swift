0f4ff9f4d097fb8c1d7256a44b724267b2c768a6
Isabella <isabellawzai@gmail.com>
2023-08-16 14:34:20 -0400

    Initial Commit

---
+++ ValidusPrototype/workoutFeature.swift
@@ -0,0 +1,20 @@
+//
+//  workoutFeature.swift
+//  ValidusPrototype
+//
+//  Created by scholar on 8/16/23.
+//
+
+import SwiftUI
+
+struct workoutFeature: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct workoutFeature_Previews: PreviewProvider {
+    static var previews: some View {
+        workoutFeature()
+    }
+}
